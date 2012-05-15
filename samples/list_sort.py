a2b930a55c003f7447ba6fd01983dea6acd4ee9f
Ryeonho Heo <heo@gu3.co.jp>
2012-05-15 17:34:26 +0900

    sort test

---
+++ samples/list_sort.py
@@ -0,0 +1,18 @@
+'''
+Some tries of sorting
+'''
+a = [3, 1, 2]
+
+print 'before sort'
+print a
+
+a.sort()
+
+print 'after sort()'
+print a
+
+
+a.sort(reverse=True)
+
+print 'after sort(reverse=True)'
+print a
